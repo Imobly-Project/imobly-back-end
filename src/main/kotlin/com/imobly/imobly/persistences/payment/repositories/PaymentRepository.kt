babce073ccb789f922b1081263906e8d79a2b79e
vitormneris <joaomoreiraneris0@gmail.com>
2025-11-29 14:30:25 -0300

    fix: update category endpoint

--- src/main/kotlin/com/imobly/imobly/persistences/payment/repositories/PaymentRepository.kt
+++ src/main/kotlin/com/imobly/imobly/persistences/payment/repositories/PaymentRepository.kt
@@ -2,5 +2,8 @@ package com.imobly.imobly.persistences.payment.repositories
 
 import com.imobly.imobly.persistences.payment.entities.PaymentEntity
 import org.springframework.data.jpa.repository.JpaRepository
+import java.util.Optional
 
-interface PaymentRepository: JpaRepository<PaymentEntity, String>
+interface PaymentRepository: JpaRepository<PaymentEntity, String> {
+    fun findByLease_Id(id: String): Optional<PaymentEntity>
+}
